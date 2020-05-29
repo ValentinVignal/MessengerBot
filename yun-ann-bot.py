53173c5ae59b72d36b721f32b28c4db55105337a
ValentinVignal <valentin.vignal.dev@outlook.fr>
2020-05-29 18:59:53 +0800

    better condition and remove prints

--- yun-ann-bot.py
+++ yun-ann-bot.py
@@ -38,59 +38,59 @@ class YunAnnBot(Client):
         bad_words = ['berlin', 'menfred', 'harry', 'style']
 
         # toggle = self.fetchThreadMessages(thread_id=self.uid, limit=1)  # Own account
-        if author_id == users['yun_ann'] and message_object.text is not None:
+        if message_object.text is not None:
             # print('author', author_id, 'message object', message_object, 'threadid', thread_id)
-            # If some bad words are said
-            text = message_object.text.lower()
-            angry = functools.reduce(lambda x, y: x or y in text, bad_words, False)
-            if angry:
-                # Answer it
-                self.sleep()
-                self.send(
-                    message=models.Message(text='幹恁娘雞掰', reply_to_id=message_object.uid),
-                    thread_id=thread_id,
-                    thread_type=thread_type,
-                )
-                # Angry react
-                self.sleep()
-                self.reactToMessage(
-                    message_id=message_object.uid,
-                    reaction=models.MessageReaction.ANGRY
-                )
-            print(langdetect.detect_langs(message_object.text))
-            if is_lang(message_object.text, 'es'):
-                # If some Spanish is written
-                self.sleep()
-                self.reactToMessage(
-                    message_id=message_object.uid,
-                    reaction=models.MessageReaction.ANGRY
-                )
-                self.sleep()
-                self.send(
-                    message=models.Message(
-                        text='我们说中文',
-                        reply_to_id=message_object.uid
-                    ),
-                    thread_id=thread_id,
-                    thread_type=thread_type,
-                )
-            elif is_lang(message_object.text, 'fr'):
-                # If some French is written
-                self.sleep()
-                self.reactToMessage(
-                    message_id=message_object.uid,
-                    reaction=models.MessageReaction.HEART
-                )
-            if 'usually' in message_object.text:
-                self.sleep()
-                self.send(
-                    message=models.Message(
-                        text='It depends',
-                        reply_to_id=message_object.uid
-                    ),
-                    thread_id=thread_id,
-                    thread_type=thread_type,
-                )
+            if author_id == users['yun_ann']:
+                # If some bad words are said
+                text = message_object.text.lower()
+                angry = functools.reduce(lambda x, y: x or y in text, bad_words, False)
+                if angry:
+                    # Answer it
+                    self.sleep()
+                    self.send(
+                        message=models.Message(text='幹恁娘雞掰', reply_to_id=message_object.uid),
+                        thread_id=thread_id,
+                        thread_type=thread_type,
+                    )
+                    # Angry react
+                    self.sleep()
+                    self.reactToMessage(
+                        message_id=message_object.uid,
+                        reaction=models.MessageReaction.ANGRY
+                    )
+                if is_lang(message_object.text, 'es'):
+                    # If some Spanish is written
+                    self.sleep()
+                    self.reactToMessage(
+                        message_id=message_object.uid,
+                        reaction=models.MessageReaction.ANGRY
+                    )
+                    self.sleep()
+                    self.send(
+                        message=models.Message(
+                            text='我们说中文',
+                            reply_to_id=message_object.uid
+                        ),
+                        thread_id=thread_id,
+                        thread_type=thread_type,
+                    )
+                elif is_lang(message_object.text, 'fr'):
+                    # If some French is written
+                    self.sleep()
+                    self.reactToMessage(
+                        message_id=message_object.uid,
+                        reaction=models.MessageReaction.HEART
+                    )
+                if 'usually' in message_object.text:
+                    self.sleep()
+                    self.send(
+                        message=models.Message(
+                            text='It depends',
+                            reply_to_id=message_object.uid
+                        ),
+                        thread_id=thread_id,
+                        thread_type=thread_type,
+                    )
 
 
 if __name__ == '__main__':
